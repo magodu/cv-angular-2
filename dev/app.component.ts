8053410821c2fc26ab4ff31f56b5c32fa1ce3920
Mario Gonzalez Duarte <mario.gonzalez@extern.isban.com>
2016-04-30 17:17:59 -0400

    add routing

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,15 +1,17 @@
 import {Component} from 'angular2/core';
+import { ROUTER_DIRECTIVES, RouteConfig } from 'angular2/router';
 import {CVComponent} from "./cv/cv.component";
 
 @Component({
     selector: 'my-app',
     template: `
-    	<my-cv></my-cv>
+    <router-outlet></router-outlet>
     `,
-    directives: [CVComponent]
+    directives: [ROUTER_DIRECTIVES]
 })
 
+@RouteConfig([
+		{ path: '/', name: 'CurriculumVitae', component: CVComponent, useAsDefault: true }
+])
 
-export class AppComponent {
-
-}
+export class AppComponent { }
--- dev/boot.ts
+++ dev/boot.ts
@@ -1,4 +1,5 @@
-import {bootstrap} from 'angular2/platform/browser'
+import {bootstrap} from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import 'rxjs/add/operator/map';
 import {AppComponent} from './app.component'
@@ -15,6 +16,7 @@ declare var jQuery: JQueryStatic;
 
 //noinspection TypeScriptValidateTypes
 bootstrap(AppComponent, [
+	ROUTER_PROVIDERS,
 	HTTP_PROVIDERS,
 	ANGULAR2_GOOGLE_MAPS_PROVIDERS,
 	HttpService

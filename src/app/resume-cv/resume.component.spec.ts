a7bd2877db19317b9bf53b8890ac8aa3dd1ddfcb
Mario Gonzalez Duarte <mario.gonzalez@extern.isban.com>
2017-04-13 18:37:52 -0400

    delete commented code

--- src/app/resume-cv/resume.component.spec.ts
+++ src/app/resume-cv/resume.component.spec.ts
@@ -1,15 +1,5 @@
 /* tslint:disable:no-unused-variable */
 
-/*require('zone.js');
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/proxy';
-import 'reflect-metadata';*/
-
-
-
 import {
     HttpModule,
     Http,
